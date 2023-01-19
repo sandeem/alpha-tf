2032841b3a921fcdd44233b5d20642cfbbea3abb
sandeem <sandeem@g.clemson.edu>
2023-01-18 22:22:57 -0500

    new ami 10:22 PM 1-18-2023

--- variables.tf
+++ variables.tf
@@ -9,5 +9,5 @@ variable "full_cidr_block" {
 variable "image_id" {
   #default ="ami-0acb95b8e72d78387"   # Ubuntu + Apache + Check.html + Java + maven
   #default = "ami-0ff39345bd62c82a5"  # Plain Ubuntu
-  default = "ami-076d802a62a0b70f0"   # AMI is from Packer Build Image Job
+  default = "ami-02f2c50de873b0ce5"   # AMI is from Packer Build Image Job
 }
